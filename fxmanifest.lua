2b14f73e59e1f1c0202d38da804101e07c641da5
MRguldDEV <139634720+McMRguldDev@users.noreply.github.com>
2026-08-07 14:23:08 +0200

    Add metadata and scripts to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
 
+author 'NovaCore-MTCore'
+description 'A panel supports vrp-esx-qbcore'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
